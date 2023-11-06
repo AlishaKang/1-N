ca273e5d8f880885f3157c89705ae477aedce62a
AlishaKang <oneoneone920920920@gmail.com>
2023-11-06 13:30:18 +0900

    Comment Create기능 구현

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm, CommentForm
+
 # Create your views here.
 def index(request):
     articles = Article.objects.all()
@@ -11,8 +13,44 @@ def index(request):
 
 def detail(request, id):
     article = Article.objects.get(id=id)
+    form = CommentForm()
 
     context = {
         'article': article,
+        'form': form,
     }
     return render(request, 'detail.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', id=article.id)
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'form.html', context)
+
+def comment_create(request, article_id):
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        # form을 저장 => 추가로 넣어야 하는 데이터를 넣기 위해서 저장 멈춰!
+        comment = form.save(commit=False)
+        
+        # 첫 번째 방법(객체를 저장하는 방법)
+        # article_id를 기준으로 article obj 가져오기
+        # article = Article.objects.get(id=article_id)
+        # article컬럼에 추가
+        # comment.article = article
+        # comment.save()
+
+        # 두 번째 방법(integer를 저장하는 방법)
+        comment.article_id = article_id
+        comment.save()
+
+        return redirect('articles:detail', id=article_id)
